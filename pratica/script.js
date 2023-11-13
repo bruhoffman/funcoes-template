dbefe31175be82a7d5b1f6269f466658be7f9a5d
bruhoffman <bruhoffman@gmail.com>
2023-11-13 13:59:00 -0300

    Exercícios de Sala de Aula

--- pratica/script.js
+++ pratica/script.js
@@ -1,1 +1,51 @@
-// Acompanhe a Prática 
+// Prática 1
+/* function imprimeNome(nome) {
+    console.log(`Olá, ${nome}!`);
+}
+
+const cidadeNome = function (cidade) {
+    console.log(`Você mora na cidade de ${cidade}.`);
+}
+
+const paisNome = (pais) => {
+    console.log(pais);
+}
+
+imprimeNome("Bruno");
+imprimeNome("Juliano");
+imprimeNome("Maria Anizia");
+
+cidadeNome("Rio Novo do Sul");
+cidadeNome("Venda Nova do Imigrante");
+cidadeNome("Belo Horizonte");
+
+paisNome("Brasil");
+paisNome("Irlanda");
+paisNome("Hong Kong"); */
+
+//PRÁTICA 2
+/* const paridade = function (num) {
+    const numero = (num % 2) === 0;
+    const soma = num + 10;
+    const multiplica = num * num;
+
+    console.log(`O número é par? ${numero}. Somado com 10 é igual a ${soma} e multiplicado por ele mesmo é ${multiplica}.`);
+}
+
+paridade(8);
+paridade(13);
+paridade(6); */
+
+//PRÁTICA 3
+/* const login = (usuario, senha) => {
+    loginArmazenado = "astrodev";
+    senhaArmazenada = "bananinha123";
+
+    const verificacao = (usuario === loginArmazenado) && (senha === senhaArmazenada);
+
+    console.log(verificacao);
+}
+
+login("Bruno", "rionovo123");
+login("Bruno", "bananinha123");
+login("astrodev", "bananinha123"); */
